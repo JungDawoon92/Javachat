711a1554106297d8f2843cc911a9001f2419fc17
tyrannojung <58019931+tyrannojung@users.noreply.github.com>
2020-03-14 00:08:35 +0900

    클래스 메소드 추가해서 업그레이드하기.

--- src/March_2020/Tighten_weapon.java
+++ src/March_2020/Tighten_weapon.java
@@ -1,17 +1,53 @@
 package March_2020;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Tighten_weapon {
 
 	public static void main(String[] args) {
 //		   double d = Math.random();
-		   
-		   
+		   Scanner sc = new Scanner(System.in);
+		   int tri;
+		   int n = 2;
 		   Random r = new Random();
+//----------------------------------------------------------		   
+		   int money = 5000;
+//		   String Buy =  "B";
+//		   String Sell =  "S";
+//----------------------------------------------------------
+		   
+		   for(int i=1; i>0; i++) {
+			   String trS =sc.nextLine();
+			   
+			   if(trS.equals("B")) {
+				   money = money - 1000;
+				   System.out.printf("용무기를 구매하셨습니다. 현재 자본금 %d \n",money);
+			   }
+			   
+			   else if(trS.equals("S")) {
+				   System.out.printf("용무기를 판매하셨습니다. 현재 자본금 %d \n", money);
+				   money = money + 1000;
+			   }
+			   else if(money == 0) {
+				   break;
+			   }
+		   }
 
-			if(r.nextInt(10)<7) {
-				System.out.println("무기강화에 성공하셨습니다.");
-			}
-			else System.out.println("실패하셨습니다.");
+		   
+//----------------------------------------------------------
+		   for(int i = 9; i>1; i--) {
+			   if(r.nextInt(10)<i) {
+					System.out.printf("무기강화에 성공하셨습니다. %d강 \n",n);
+					System.out.printf("%d에 도전하시겠습니까?(1번입력) 확률 %d%%",n+1,(10-n)*10);
+					tri =sc.nextInt();
+					if(tri == 1) {
+					}
+				}
+				else {
+					System.out.println("실패하셨습니다.");
+					break;
+				}
+			   n++;
+		   }
 	}
 }
