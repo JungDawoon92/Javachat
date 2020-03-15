da50f4d0a407fe2bb61f4527210f782cd4364921
tyrannojung <58019931+tyrannojung@users.noreply.github.com>
2020-03-15 14:46:42 +0900

    용무기강화

--- src/March_2020/Tighten_weapon.java
+++ src/March_2020/Tighten_weapon.java
@@ -2,7 +2,8 @@ package March_2020;
 import java.util.Random;
 import java.util.Scanner;
 
-// 다음에 해야할것. 판매하기기능 + 강화시 뜨레드로 지연하기 기능
+// 다음에 해야할것. 강화시 뜨레드로 지연하기 기능 + 0원일때 게임종료
+
 
 class Good {
 	int tri;
@@ -13,21 +14,33 @@ class Good {
 	public void Goodgame() {
 		
 		int n = 2;
+		double a =1;
+		double fac=1;
+		
 		money = money-1000;
 		System.out.printf("남은돈: %d \n",money);
 		
 		for(int i = 9; i>1; i--) {
-
+			
+			a = i*0.1;
+			fac = fac*a;
+			
 			if(r.nextInt(10)<i) {
+				int sell = ((1000+((int)(4000*(1-(fac)))))/100)*100;
+				
 				System.out.printf("무기강화에 성공하셨습니다. %d강 \n",n);
-				System.out.printf("%d에 도전하시겠습니까?(1번입력) 확률 %d%%",n+1,(10-n)*10);
+				System.out.printf("%d에 도전하시겠습니까?(도전 : 1번입력) 확률 %d%% \n",n+1,(10-n)*10);
+				System.out.printf("판매금액: %d원(판매 : 2번입력)  \n",sell);
+				
 				tri =sc.nextInt();
+				
 				if(tri == 1) {
 				}
+				
 				else if(tri == 2){
-					System.out.printf("용무기를 판매하셨습니다. 현재 자본금 %d \n", money);
-					   money = money + 1000;
-					   break;
+					System.out.printf("용무기를 판매하셨습니다.  \n");
+					money = money +sell;
+					break;
 				}
 			}
 			else {
@@ -39,7 +52,6 @@ class Good {
 			
 		}
 	}
-
 
 
 public class Tighten_weapon {
